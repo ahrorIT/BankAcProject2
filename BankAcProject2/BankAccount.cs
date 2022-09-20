db6867c6f38b95f743943278c13a5a92684bdf0a
ahrorwevdesign <ahroraka@gmail.com>
2022-09-20 12:19:22 -0400

    Add project files.

---
+++ BankAcProject2/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAcProject2
+{
+    public class BankAccount
+    {
+        private string _firstname;
+        private string _lastname;
+
+        public decimal Balance
+        {
+            get; set;
+        }
+
+        public BankAccount(string fname, string lname, decimal initial = 0.0m)
+        {
+            _firstname = fname;
+            _lastname = lname;
+            Balance = initial;
+        }
+
+        public string AccountOwner
+        {
+            get => $"{_firstname} {_lastname}";
+        }
+
+        public virtual void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public virtual void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
---
+++ BankAcProject2/ChekingAcct.cs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAcProject2
+{
+    public class CheckingAcct : BankAccount
+    {
+        private const decimal OVERDRAW_CHARGE = 35.0m;
+
+        public CheckingAcct(string fname, string lname, decimal initial)
+            : base(fname, lname, initial)
+        {
+        }
+
+        public override void Withdraw(decimal amount)
+        {
+            // if the account gets overdrawn add an extra charge
+            if (amount > Balance)
+            {
+                amount += OVERDRAW_CHARGE;
+            }
+            base.Withdraw(amount);
+        }
+    }
+}
